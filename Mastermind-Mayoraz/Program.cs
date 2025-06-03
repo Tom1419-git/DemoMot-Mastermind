4efbb94f9547a93e449c7a4a59e58992e4527442
Thomas Mayoraz <thomas.mayoraz@eduvaud.ch>
2025-06-03 08:33:42 +0200

    Création du dossier de mon code

---
+++ Mastermind-Mayoraz/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Title: Mastermind Game
+* Autor: Thomas Mayoraz
+* Last Updated: 03.06.2025
+* Description: Ce programme permettra à l'utilisateur de jouer au mastermind seul ou contre un ami 
+*              
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind_Mayoraz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
